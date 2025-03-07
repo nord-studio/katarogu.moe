b5215ad908a06c24a5b87bba87b3b96f3c01605a
Ty Mason <hi@tygr.dev>
2025-03-07 16:47:55 +0000

    fix: create fallback url for minio

--- lib/minio.ts
+++ lib/minio.ts
@@ -1,8 +1,8 @@
 import * as Minio from "minio"
 
 const minio = new Minio.Client({
-	endPoint: process.env.NODE_ENV === "production" ? process.env.S3_ENDPOINT! : "127.0.0.1",
-	port: process.env.NODE_ENV === "production" ? parseInt(process.env.S3_PORT!) : 9000,
+	endPoint: process.env.S3_ENDPOINT ?? "127.0.0.1",
+	port: process.env.S3_PORT ? parseInt(process.env.S3_PORT) : 9000,
 	useSSL: process.env.S3_SECURE === "true",
 	accessKey: process.env.S3_ACCESS_KEY!,
 	secretKey: process.env.S3_SECRET_KEY!
